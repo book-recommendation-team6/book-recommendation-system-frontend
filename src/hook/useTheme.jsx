0ceb93606c27879a80b0cd5aded43042213d70a3
DUYBEGINER <phucduygg8485@gmail.com>
2025-10-10 18:17:54 +0700

    create hook useTheme to change light/dark mode

---
+++ src/hook/useTheme.jsx
@@ -0,0 +1,54 @@
+import {useState, useEffect} from 'react';
+
+const useTheme = () => {
+    const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'system');
+    const element = document.documentElement;
+    const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    console.log(`Current theme: ${theme}`);
+
+    const onWindowMatch = () => {
+        if( localStorage.theme === "dark" || (!("theme" in localStorage) && darkQuery.matches)) {
+            element.classList.add('dark');
+        } else {
+            element.classList.remove('dark');
+        }
+    }
+
+    useEffect(() => {
+       switch(theme) {
+            case 'dark':
+                element.classList.add('dark');
+                localStorage.setItem('theme', 'dark');
+                break;
+            case 'light':
+                element.classList.remove('dark');
+                localStorage.setItem('theme', 'light');
+                break;
+            default:
+                localStorage.removeItem('theme');
+                onWindowMatch(darkQuery);
+                break;
+        }
+    }, [theme]);
+
+    useEffect(() =>{
+        const changeHandler = e => {
+            if(!("theme" in localStorage)) {
+                if(e.matches) {
+                    element.classList.add('dark');
+                } else {
+                    element.classList.remove('dark');
+                }
+            }
+        }
+
+        darkQuery.addEventListener('change', changeHandler);
+        return () => {
+            darkQuery.removeEventListener('change', changeHandler);
+        };
+    }, [])
+
+    return [theme, setTheme];
+}
+
+export default useTheme;
