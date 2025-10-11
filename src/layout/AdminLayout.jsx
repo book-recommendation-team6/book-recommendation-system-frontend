ae1bad0cba59414beb3f7fda581e0f17a68be7f3
DUYBEGINER <phucduygg8485@gmail.com>
2025-10-11 12:03:32 +0700

    feat(Admin): create Admin Layout

---
+++ src/layout/AdminLayout.jsx
@@ -0,0 +1,16 @@
+import AdminSidebar from "../components/admin/AdminSidebar"
+import AdminHeader from "../components/admin/AdminHeader"
+
+const AdminLayout = ({ children, title }) => {
+  return (
+    <div className="flex min-h-screen bg-gray-50 dark:bg-slate-950">
+      <AdminSidebar />
+      <div className="flex-1 flex flex-col">
+        <AdminHeader title={title} />
+        <main className="flex-1 p-8">{children}</main>
+      </div>
+    </div>
+  )
+}
+
+export default AdminLayout
